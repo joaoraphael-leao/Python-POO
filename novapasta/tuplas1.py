df8e6ad0800753c12672dec86dab847afacfb9ed
Joao Raphael Leão <142404597+joaorleao@users.noreply.github.com>
2024-05-27 20:03:05 -0300

    exercicios

---
+++ novapasta/tuplas1.py
@@ -0,0 +1,5 @@
+tupla = ("rapha", "davi", "eneas", "milton", "gustavo", "mariah", "bruno")
+
+print(f"Primeiro nome: {tupla[0]}")
+
+print(f"Ultimo nome: {tupla[-1]}")
---
+++ novapasta/tuplas2.py
@@ -0,0 +1,5 @@
+tupla = (3, 4, 5, 3, 3, 3, 6, 6)
+
+repeticoes = tupla.count(3)
+
+print(f"O numero 3 apareceu {repeticoes} vezes")
